4df80ba64e632e70672eaf3a744fc4d4f1c8070b
吴文豪 <529280602@qq.com>
2016-08-13 23:37:15 +0800

    新增微信接入

--- src/test/java/com/thinkgem/jeesite/test/Test.java
+++ src/test/java/com/thinkgem/jeesite/test/Test.java
@@ -1,8 +1,10 @@
 package com.thinkgem.jeesite.test;
 
+import com.thinkgem.jeesite.common.config.Global;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
+		System.out.println(Global.getProjectPath());
 	}
 }
